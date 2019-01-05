183b1fca4959d5f799768458b3ce8e6bb6f1ed5c
ewox125 <46382908+ewox125@users.noreply.github.com>
2019-01-05 10:57:42 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBinvcr9OJbb_bmJa8ioehfnmnzISmfpizm5NlkaO3k4CKe5e7vZKktNuK3K2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+
+$meid = "b175d5a9b4b287c4";
+
+$os="android";
+
+$sign="19adbfca705fd7dab8c788e0b3061db219b02fd2803007f2d8b11fae9ed7a7fe";
